d375752a90b5c8e1d2f4ee508edafea35dfb94fb
Sixterk <lavandarocanokenneth@gmail.com>
2025-09-10 20:18:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middlewares
 app.use(express.json());
